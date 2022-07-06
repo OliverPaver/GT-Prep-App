58b665c80df8e0a6c76d523fd95ec8a47dbd0569
Nigel Paver <ncpaver@icloud.com>
2022-07-06 18:18:20 -0500

    Initial Commit

---
+++ GT Prep App/GT_Prep_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GT_Prep_AppApp.swift
+//  GT Prep App
+//
+//  Created by Nigel C Paver on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct GT_Prep_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
